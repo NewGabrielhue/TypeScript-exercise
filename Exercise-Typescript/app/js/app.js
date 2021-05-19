3ef353a58f971f6aa7b90ccf9b9df4270b15385a
NewGabrielhue <72609618+NewGabrielhue@users.noreply.github.com>
2021-05-19 17:47:05 -0300

    new

--- Exercise-Typescript/app/js/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var contabancaria_1 = require("./classes/contabancaria");
-var contaBancaria = new contabancaria_1.default("Gabriel Vitor", "Exemplo", "Natal", "Candelária", "105");
-contaBancaria.informacoesConta();
